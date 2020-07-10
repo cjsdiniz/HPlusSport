9d1ddd25695c4ae328c0eefb45426757b0316eb1
cjsdiniz <carlos.diniz@syone.com>
2020-07-10 17:14:12 +0100

    Initial

---
+++ HPlusSport.API/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HPlusSport.API.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string   Email { get; set; }
+        public string   Name { get; set; }
+        public virtual List<Order> Orders { get; set; }
+        public virtual List<Product> Products { get; set; }
+    }
+}
